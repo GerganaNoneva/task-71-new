fdcfd6f2eaef29937179f57a62645e333375ed07
GerganaNoneva <gergananoneva@gmail.com>
2023-01-30 17:00:03 +0200

    Change

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [text, setText] = useState('');
-
+/*
   useEffect(() => {
       fetch('https://jaspervdj.be/lorem-markdownum/markdown.txt')
       .then(res => res.text())
       .then(setText);
   }, []);
-
+*/
+useEffect(() => {
+    fetch("https://jaspervdj.be/lorem-markdownum/markdown.txt")
+    .then(response => response.text())
+    .then(response => setText(response));
+  },[]);
   return (
     <div>
       <Document title="Terms and Conditions" content={text} />
